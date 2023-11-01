b468b2619d5f5942a94c51d9516d4eb367e5f77d
aliabdelmohsen <aliabdelmohsen1@gmail.com>
2023-11-01 10:53:47 +0200

    OrderTask handle spring Security Exceptions

--- shopping/src/main/java/com/qeema/shopping/exception/ApiExceptionHandler.java
+++ shopping/src/main/java/com/qeema/shopping/exception/ApiExceptionHandler.java
@@ -1,20 +1,30 @@
 package com.qeema.shopping.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.csrf.InvalidCsrfTokenException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @ControllerAdvice
 public class ApiExceptionHandler {
+    @Autowired
+    private MessageSource messageSource;
+
     @ExceptionHandler(CustomValidationException.class)
     public ResponseEntity<ErrorResponse> handleCustomValidationException(CustomValidationException ex) {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
@@ -30,14 +40,27 @@ public class ApiExceptionHandler {
                 new ErrorResponse(httpStatus, ex.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(errorResponse, httpStatus);
     }
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException ex) {
+
+    @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class
+            , InvalidCsrfTokenException.class, AuthenticationException.class})
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(Exception ex) {
         HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
         ErrorResponse errorResponse =
-                new ErrorResponse(httpStatus, ex.getMessage(), LocalDateTime.now());
+                new ErrorResponse(httpStatus,
+                        messageSource.getMessage("USER.AUTHENTICATION_FAILED", null, Locale.US),
+                        LocalDateTime.now());
         return new ResponseEntity<>(errorResponse, httpStatus);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException ex) {
+        HttpStatus httpStatus = HttpStatus.FORBIDDEN;
+        ErrorResponse errorResponse =
+                new ErrorResponse(httpStatus,
+                        messageSource.getMessage("USER.NO_PERMISSION", null, Locale.US),
+                        LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, httpStatus);
+    }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         List<String> errors = new ArrayList<>();
